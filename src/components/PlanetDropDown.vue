c7a5999b0652095fb87ff1f732bf2932240a48d6
Alex Redfearn <alex@goodlpay.com>
2020-03-19 12:38:12 +0000

    Add planet drop down list

---
+++ src/components/PlanetDropDown.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <select id="planets">
+            <option v-for="planet in planets" :key="planet.id" :value="planet.id">
+                {{planet.name}}
+            </option>
+        </select>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "planet-drop-down",
+        props: {
+            planets: Array
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
