d6f1ea93c8b7ce20a02ac2d5308b1ca232db8a38
Alex Redfearn <alex@goodlpay.com>
2020-03-19 11:32:49 +0000

    Add scale in meters

---
+++ src/components/HeightChart.vue
@@ -0,0 +1,46 @@
+<template>
+    <div id="height-table">
+        <table>
+            <tbody>
+            <tr v-for="i in reversedItems" :key="i">
+                <td>
+                    {{i.toString() + "m"}}
+                </td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "height-chart",
+        props: {
+            meters: {
+                type: Array
+            }
+        },
+        computed: {
+            reversedItems() {
+                var max = this.meters.length;
+                var arr = [];
+
+                for(var i = max; i >= 0; i--){
+                    arr.push(i)
+                }
+
+                return arr;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    table, th, td {
+        border: none;
+        border-collapse:separate;
+        border-spacing:0 70px;
+        color: white;
+    }
+
+</style>
